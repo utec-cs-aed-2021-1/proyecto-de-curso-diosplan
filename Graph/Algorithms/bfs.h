cbe413db79b45a3e971e6faa4fef8e632bb06c7a
jose <jose.sanchez.a@utec.edu.pe>
2021-07-03 16:34:16 -0500

    avance bfs

--- Graph/Algorithms/bfs.h
+++ Graph/Algorithms/bfs.h
@@ -17,7 +17,35 @@ BFS<TV, TE>::BFS(UnDirectedGraph<TV, TE> *grafo_)
 }
 
 template<typename TV, typename TE>
-UnDirectedGraph<TV, TE> BFS<TV, TE>::aplicar(string source)
-{
-    
+UnDirectedGraph<TV, TE> BFS<TV, TE>::aplicar(string source) {
+    UnDirectedGraph<TV, TE> result;
+    std::queue<Vertex<TV, TE>*> Q;
+    std::unordered_map<TV, std::pair<bool, string>> visitados;
+    for (auto i : this->grafo->vertexes)
+    {
+        visitados[i.second->data].first = false;
+        visitados[i.second->data].second = i.first;
+        result.insertVertex(i.first, i.second->data);
+    }
+    visitados[this->grafo->vertexes.begin()->second->data].first = true;
+    if (this->grafo->findById(source))
+    {
+        Q.push(this->graph->vertexes[source]);
+    }
+    while (!Q.empty())
+    {
+        auto temp = Q.front();
+        Q.pop();
+        for (auto j : temp->edges)
+        {
+            if (visitados[j->vertexes[1]->data].first == false)
+            {
+                Q.push(j->vertexes[1]);
+                visitados[j->vertexes[1]->data].first = true;
+                result.createEdge(visitados[j->vertexes[0]->data].second, visitados[j->vertexes[1]->data].second, j->weight);
+            }
+        }
+    }
+
+    return result;
 }
